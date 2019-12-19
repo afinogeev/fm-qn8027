bd488634597be212175ea1ec4b1d5910da44d572
Rustik <afinogeevrv@gmail.com>
2019-12-19 10:58:18 +0300

    from pi

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
+#-*- coding: utf-8 -*-
 
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSignal, QObject
 import widget
 import sys
 import time
-import fm
-import temperature
+from ds18b20 import DS18B20
 import settings
 from threading import Event, Thread
+import qn8027
 
 path_settings = "settings.ini" # Файл настроек
 
 # Communication temperatureThread with UiApp
 class Communicate(QObject):
     signalT = pyqtSignal(str)
+    signalRds = pyqtSignal(str)
 commun = Communicate()
 
 ## UI
@@ -34,47 +36,95 @@ class UiApp(QtWidgets.QWidget, widget.Ui_Form):
         self.doubleSpinBoxPow.valueChanged.connect(self.power)
         self.pushButtonRds.clicked.connect(self.rds)
         self.pushButtonTerminal.clicked.connect(self.terminal)
+        
+        # QN8027
+        qn8027.init()
+        self.freq()
+        self.power()
+        self.rds()
+        
+        #OVERHEAT
+        self.overheat = False
     
     def freq(self):
         f = self.doubleSpinBoxFreq.value()
+        qn8027.setFrequency(int(self.doubleSpinBoxFreq.value()*100))
         self.labelStatus.setText("freq: " + str(f) +  "MHz")
-        settings.update_setting(path_settings, 'Settings', "freq", str(f))
-        
+        settings.update_setting(path_settings, 'Settings', "freq", str(f))        
     
     def power(self):
         p = self.doubleSpinBoxPow.value()
+        qn8027.setPower(self.doubleSpinBoxPow.value())
         self.labelStatus.setText("power: " + str(p)+  "%")
         settings.update_setting(path_settings, 'Settings', "power", str(p))
     
     def rds(self):
         text = self.lineEditRds.text()
+        qn8027.setRDS(text)
+        if(len(text)>8):
+            text = text[0:8:1]
+        else:
+            text = text.center(8," ")
+        commun.signalRds.emit(text)
         self.labelStatus.setText("rds: " + text)
         settings.update_setting(path_settings, 'Settings', "rds", text)
         
     
     def terminal(self):
-        text = text = self.lineEditTerminal.text()
-        self.labelStatus.setText("terminal: " + text)
+        text = self.lineEditTerminal.text().split(",")
+        if(len(text)==3):
+            reg = int(text[0])
+            mask = int(text[1])
+            data = int(text[2])
+            #print(reg)
+            #print(mask)
+            #print(data)
+            qn8027.writeData(reg, mask, data)
+        self.labelStatus.setText("Terminal: "+str(qn8027.readData(reg, 0xFF)))
+        
     
     def hello(self, text):
         self.labelStatus.setText(text)
         
     def temperature(self, text):
         self.labelTemp.setText(text)
+        if(float(text)>=70. and self.overheat==False):
+            qn8027.setPower(0.)
+            self.overheat = True
+        if(float(text)<70. and self.overheat):
+            qn8027.setPower(self.doubleSpinBoxPow.value())
+            self.overheat = False
+            
         
-# TEMPERATURE THREAD
+        
+#TEMPERATURE THREAD
 class temperatureThread(Thread):
     def __init__(self, event):
         Thread.__init__(self)
         self.stopped = event
-        self.cnt = 0
+        self.ds18b20 = DS18B20()
 
     def run(self):
         while not self.stopped.wait(5):
-            self.cnt+=1
-            commun.signalT.emit(str(self.cnt))
+            commun.signalT.emit(str(self.ds18b20.tempC(0)))
 temperatureTstop = Event()
 temperatureT = temperatureThread(temperatureTstop)
+
+#RDS THREAD
+class rdsThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+        self.text = ""
+        commun.signalRds.connect(self.setText)
+
+    def run(self):
+        while not self.stopped.wait(9):
+            qn8027.setRDS(self.text)
+    def setText(self, textui):
+        self.text = textui   
+rdsTstop = Event()
+rdsT = rdsThread(rdsTstop)
 
 ###
 def main():
@@ -82,8 +132,10 @@ def main():
     window = UiApp()  # Создаём объект 
     window.show()  # Показываем окно 
     temperatureT.start()
+    rdsT.start()
     app.exec_()  # и запускаем приложение
     temperatureTstop.set()
+    rdsTstop.set()
     sys.exit()
 
     
--- qn8027.py
+++ qn8027.py
@@ -1,20 +1,33 @@
-#!/usr/bin/python
 #module used to control the qn8027 fm transmitter
 
-#import smbus
-import settings
+import smbus
 import time
 
-currentFreq = 0
+currentFreq = 8800
 maxFreq = 10790
 minFreq = 7610
 
-#bus = smbus.SMBus(1)    #we are using i2c port 1
+bus = smbus.SMBus(1)    #we are using i2c port 1
 DEVICE_ADDRESS = 0x2C      #7 bit address (will be left shifted to add the read write bit)
 
 SYSTEM_REG = 0x00       #system register
 CH1_REG = 0x01          #register that the lower 8 bits of the frequency goes in
 GPLT_REG = 0x02
+XTL_REG = 0x03
+VGA_REG = 0x04
+CID1_REG =  0x05
+CID2_REG =  0x06
+STATUS_REG = 0x07
+RDSD0_REG = 0x08
+RDSD1_REG = 0x09
+RDSD2_REG = 0x0A
+RDSD3_REG = 0x0B
+RDSD4_REG = 0x0C
+RDSD5_REG = 0x0D
+RDSD6_REG = 0x0E
+RDSD7_REG = 0x0F
+PAC_REG = 0x10
+FDEV_REG = 0x11
 RDS_REG = 0x12
 
 SWRST = 0x80
@@ -32,16 +45,16 @@ PAOFF_MASK = 0x30
 def init():
     #TRY CONNECTING TO THE DEVICE FIRST
 
-
+    print("QN8027 initialization")
     global currentFreq
-    currentFreq = settings.saveData['frequency']
     writeData(SYSTEM_REG, SWRST, SWRST)     #reset all registers
-    writeData(0x04, 0x03, 0x01)
-    #writeData(0x11, 0xFF, 0xF0)
+    writeData(VGA_REG, 0x0F, 0x09)
+    #writeData(FDEV_REG, 0xFF, 0x80)
+    writeData(RDS_REG, 0x80, 0x80)
     recalibrate()
-    writeData(GPLT_REG, PAOFF_MASK, PAOFF_MASK)     #tell the device that it shouldn't go into standby if there is no audio
+    writeData(GPLT_REG, PAOFF_MASK, 0x30)     #tell the device that it shouldn't go into standby if there is no audio
+    writeData(GPLT_REG, 0x0F, 0x07)
     setFrequency(currentFreq)
-    print("Transmitting on: " + str(getFrequency()))
     enableTransmit(True)
 
 
@@ -54,22 +67,26 @@ def recalibrate():
 #sets the frequency, give it a frequency formatted like 10310 for 103.1MHz, or 9890 for 98.9MHz
 #make sure the frequency is an odd numbered 200KHz slice, and not something like 98.4MHz
 def setFrequency(freq):
+    global currentFreq
+    currentFreq = freq
     freq = (freq - 7600) / 5    #convert the frequency to a 10 bit decimal
     #we need 10 bits because the device wants 2 bits to go in on register, and the other 8 in another register
     #it's strange but whatever
-    partOne = freq >> 8     #shift it to the right 8 times, this leaves us with the first 2 bits
-    partTwo = freq & 0xFF   #AND it with a mask covering the last 8 bits, leaving the last 8 bits
+    partOne = int(freq) >> 8     #shift it to the right 8 times, this leaves us with the first 2 bits
+    partTwo = int(freq) & 0xFF   #AND it with a mask covering the last 8 bits, leaving the last 8 bits
     
     #write out the parts to their specific registers
     writeData(SYSTEM_REG, CH0_MASK, partOne)
     writeData(CH1_REG, CH1_MASK, partTwo)
+    
+    print("Transmitting on: " + str(getFrequency()))
 
 
 #returns the current frequency set on the device formatted like 10310 for 103.1MHz
 def getFrequency():
     partOne = bus.read_byte_data(DEVICE_ADDRESS, SYSTEM_REG)    #get the first part out of the system register
     partOne = partOne & CH0_MASK    #mask out the two bits that we want
-    #partTwo = bus.read_byte_data(DEVICE_ADDRESS, CH1_REG)   #get the second part out, no need to apply any masks
+    partTwo = bus.read_byte_data(DEVICE_ADDRESS, CH1_REG)   #get the second part out, no need to apply any masks
     
     combined = (partOne << 8) | partTwo
     #shift the first part left two, and then OR them together, leaving a 10 bit value
@@ -93,10 +110,82 @@ def writeData(register, bitmask, data):
     output = currentData & (~ bitmask)  #AND the current data with the NOTed bitmask
     output = output | (data & bitmask)  #AND the new data with the bitmask, and then OR it with the current output      
     
-    #bus.write_byte_data(DEVICE_ADDRESS, register, output)       #write out the data
+    bus.write_byte_data(DEVICE_ADDRESS, register, output)       #write out the data
 
+#reads bits from register
+def readData(register, bitmask):
+    currentData = bus.read_byte_data(DEVICE_ADDRESS, register)
+    return currentData & bitmask
 
-# def nextFreq():
-
-
-# def prevFreq():
+#sets RDS data
+def setRDS(data):
+    if(len(data)>8):
+        data = data[0:8:1]
+    else:
+        data = data.center(8," ")
+    data_b = bytes(data, "utf-8")
+    #print(data)
+    i=0
+    for j in range(0,40):
+        time.sleep(0.05)
+        status = readData(STATUS_REG,8)
+        #print(status)
+        #writing A0 rds group
+        if(status==8):
+            i+=1
+            if(i==1):
+                writeData(RDSD0_REG, 0xFF, 0b01110000)
+                writeData(RDSD1_REG, 0xFF, 0b00000000)
+                writeData(RDSD2_REG, 0xFF, 0b00000101)
+                writeData(RDSD3_REG, 0xFF, 0b01100100)
+                writeData(RDSD4_REG, 0xFF, 0b00000000)
+                writeData(RDSD5_REG, 0xFF, 0b00000000)
+                writeData(RDSD6_REG, 0xFF, data_b[0])
+                writeData(RDSD7_REG, 0xFF, data_b[1])
+            if(i==2):
+                writeData(RDSD0_REG, 0xFF, 0b01110000)
+                writeData(RDSD1_REG, 0xFF, 0b00000000)
+                writeData(RDSD2_REG, 0xFF, 0b00000101)
+                writeData(RDSD3_REG, 0xFF, 0b01100001)
+                writeData(RDSD4_REG, 0xFF, 0b00000000)
+                writeData(RDSD5_REG, 0xFF, 0b00000000)
+                writeData(RDSD6_REG, 0xFF, data_b[2])
+                writeData(RDSD7_REG, 0xFF, data_b[3])
+            if(i==3):
+                writeData(RDSD0_REG, 0xFF, 0b01110000)
+                writeData(RDSD1_REG, 0xFF, 0b00000000)
+                writeData(RDSD2_REG, 0xFF, 0b00000110)
+                writeData(RDSD3_REG, 0xFF, 0b01100010)
+                writeData(RDSD4_REG, 0xFF, 0b00000000)
+                writeData(RDSD5_REG, 0xFF, 0b00000000)
+                writeData(RDSD6_REG, 0xFF, data_b[4])
+                writeData(RDSD7_REG, 0xFF, data_b[5])
+            if(i==4):
+                writeData(RDSD0_REG, 0xFF, 0b01110000)
+                writeData(RDSD1_REG, 0xFF, 0b00000000)
+                writeData(RDSD2_REG, 0xFF, 0b00000110)
+                writeData(RDSD3_REG, 0xFF, 0b01100111)
+                writeData(RDSD4_REG, 0xFF, 0b00000000)
+                writeData(RDSD5_REG, 0xFF, 0b00000000)
+                writeData(RDSD6_REG, 0xFF, data_b[6])
+                writeData(RDSD7_REG, 0xFF, data_b[7])
+            if(i==5):
+                #print('rds done')
+                break
+        rdy = readData(SYSTEM_REG, 4)
+        if(rdy==0b00000100):
+            writeData(SYSTEM_REG, 4, 0) #RDSRDY=0
+        else:
+            writeData(SYSTEM_REG, 4, 4) #RDSRDY=1
+    
+    
+#sets TX power
+def setPower(power_p):
+    power_n = int(1.27*power_p)
+    enableTransmit(False)
+    writeData(PAC_REG, 0x7F, power_n)
+    recalibrate()
+    enableTransmit(True)
+    print("Power: " + str(power_n))
+"""
+"""
